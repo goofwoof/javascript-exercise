44c71c78867d68bff7a93184834a2b1202ad820a
goofwoof <heli0086@outlook.com>
2020-08-10 23:06:52 +0800

    4 submit

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -1,1 +1,15 @@
 // TODO 16: 在这里写实现代码
+import Person from './person';
+
+class Students extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `${super.introduce()} I am a Student. I am at Class ${this.klass}.`;
+  }
+}
+
+module.exports = Students;
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,17 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I teach ${
+      this.klass === undefined ? 'No Class' : `Class ${this.klass}`
+    }.`;
+  }
+}
+
+module.exports = Teacher;
